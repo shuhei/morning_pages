2844c5ee403ca20776990875c009df8858ead900
Shuhei Kagawa <shuhei.kagawa@gmail.com>
2013-12-08 17:35:47 +0900

    Add Facebook OAuth handlers.

--- server.go
+++ server.go
@@ -4,12 +4,15 @@ import (
   "fmt"
   "log"
   "regexp"
+  "io/ioutil"
   "os"
   "os/signal"
   "time"
   "html/template"
   "net/http"
+  "net/url"
   "unicode/utf8"
+  "encoding/json"
   "labix.org/v2/mgo"
   "labix.org/v2/mgo/bson"
   "github.com/joho/godotenv"
@@ -55,7 +58,7 @@ func prepareTemplates(filenames ...string) TemplateMap {
   return tmpls
 }
 
-var templates = prepareTemplates("edit.html", "view.html")
+var templates = prepareTemplates("edit.html", "view.html", "auth.html")
 
 func renderTemplate(w http.ResponseWriter, tmpl string, p *Entry) {
   err := templates[tmpl + ".html"].ExecuteTemplate(w, "layout", p)
@@ -114,10 +117,73 @@ func staticHandler(w http.ResponseWriter, r *http.Request) {
   http.ServeFile(w, r, "public" + r.URL.Path)
 }
 
+var redirectUrl = "http://localhost:5000/auth/callback"
+
+func authHandler(w http.ResponseWriter, r *http.Request) {
+  appId := os.Getenv("FB_APP_ID")
+  dialogUrl := fmt.Sprintf("https://www.facebook.com/dialog/oauth?client_id=%s&redirect_uri=%s", appId, redirectUrl)
+  err := templates["auth.html"].ExecuteTemplate(w, "layout", dialogUrl)
+  if err != nil {
+    http.Error(w, err.Error(), http.StatusInternalServerError)
+    return
+  }
+}
+
+func authCallbackHandler(w http.ResponseWriter, r *http.Request) {
+  // TODO: Handle the case user cancelled logging in.
+  appId := os.Getenv("FB_APP_ID")
+  appSecret := os.Getenv("FB_APP_SECRET")
+  code := r.URL.Query()["code"][0]
+
+  // Get access token with the code.
+  tokenUrl := fmt.Sprintf("https://graph.facebook.com/oauth/access_token?client_id=%s&redirect_uri=%s&client_secret=%s&code=%s", appId, redirectUrl, appSecret, code)
+  res, err := http.Get(tokenUrl)
+  if err != nil {
+    log.Println("Failed to request access token from Facebook")
+    http.Error(w, err.Error(), http.StatusInternalServerError)
+    return
+  }
+  defer res.Body.Close()
+
+  resBody, _ := ioutil.ReadAll(res.Body)
+  body := string(resBody)
+  if res.StatusCode != 200 {
+    log.Println("Failed to get access token", body)
+    http.Error(w, "Failed to get access token from Facebook.", http.StatusInternalServerError)
+    return
+  }
+
+  // Find access token in the response body.
+  params, _ := url.ParseQuery(body)
+  token := params["access_token"][0]
+
+  // Get user info with the token.
+  myUrl := fmt.Sprintf("https://graph.facebook.com/me?access_token=%s", token)
+  myRes, err := http.Get(myUrl)
+  if err != nil {
+    log.Println("Failed to request user information from Facebook")
+    http.Error(w, err.Error(), http.StatusInternalServerError)
+    return 
+  }
+  defer myRes.Body.Close()
+
+  myResBody, _ := ioutil.ReadAll(myRes.Body)
+
+  myInfo := make(map[string]interface{})
+  err = json.Unmarshal(myResBody, &myInfo)
+
+  myName := myInfo["name"]
+  log.Println(myName)
+
+  http.Redirect(w, r, "/", http.StatusFound)
+}
+
 func prepareRouter(entries *mgo.Collection) *mux.Router {
   r := mux.NewRouter()
   datePattern := "{date:[0-9]+-[0-9]+-[0-9]+}"
   r.HandleFunc("/", rootHandler).Methods("GET")
+  r.HandleFunc("/auth", authHandler)
+  r.HandleFunc("/auth/callback", authCallbackHandler)
   r.HandleFunc("/entries/" + datePattern, makeHandler(viewHandler, entries)).Methods("GET")
   r.HandleFunc("/entries/" + datePattern, makeHandler(saveHandler, entries)).Methods("POST")
   r.HandleFunc("/entries/" + datePattern + "/edit", makeHandler(editHandler, entries)).Methods("GET")
