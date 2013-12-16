89ad80406a8ad800ae4b787804a95c9cab6c3cd7
Shuhei Kagawa <shuhei.kagawa@gmail.com>
2013-12-16 21:57:28 +0900

    Add tests.

--- handler_test.go
+++ handler_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/codegangsta/martini"
 	"net/http"
 	"net/http/httptest"
 	"regexp"
@@ -25,5 +26,35 @@ func Test_rootHandler(t *testing.T) {
 	matched, _ := regexp.MatchString(pattern, loc)
 	if !matched {
 		t.Errorf("Expected %s to match %s", loc, pattern)
+	}
+}
+
+func Test_validateDate_invalid(t *testing.T) {
+	w := httptest.NewRecorder()
+	r, err := http.NewRequest("", "", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	p := martini.Params{"date": "2013-1-1"}
+	validateDate(w, r, p)
+
+	badRequest := 400
+	if w.Code != badRequest {
+		t.Errorf("Expected %d but got %d", badRequest, w.Code)
+	}
+}
+
+func Test_validateDate_valid(t *testing.T) {
+	w := httptest.NewRecorder()
+	r, err := http.NewRequest("", "", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	p := martini.Params{"date": "2013-01-01"}
+	validateDate(w, r, p)
+
+	badRequest := 200
+	if w.Code != badRequest {
+		t.Errorf("Expected %d but got %d", badRequest, w.Code)
 	}
 }
--- tmpl_test.go
+++ tmpl_test.go
@@ -4,6 +4,24 @@ import (
 	"testing"
 )
 
+func Test_charCount(t *testing.T) {
+	str := "hello\r\n\r\nworld\n!"
+	expected := 14
+	count := charCount(str)
+	if count != expected {
+		t.Errorf("Expected %d but got %d", expected, count)
+	}
+}
+
+func Test_linebreak(t *testing.T) {
+	str := "hello\r\n\r\nworld\n!"
+	expected := "hello<br><br>world<br>!"
+	broken := linebreak(str)
+	if broken != expected {
+		t.Errorf("Expected %s but got %s", expected, broken)
+	}
+}
+
 func Test_extractDay_nopad(t *testing.T) {
 	date := "2013-04-14"
 	expected := "14"
